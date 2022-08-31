432c2a1b58d7680e615d0ea4938dcc341705e52a
SanCarlos25 <carlos.sanchez3@mavs.uta.edu>
2022-08-31 13:18:03 -0500

    Part 1 Completed
    User can now view the current movies that are showing in theaters. It will show the movie poster, title, and an overview.

---
+++ app/src/main/java/com/example/flixsterapplication/Adapters/MovieAdapter.java
@@ -0,0 +1,91 @@
+package com.example.flixsterapplication.Adapters;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.flixsterapplication.Models.Movie;
+import com.example.flixsterapplication.R;
+
+import java.util.List;
+
+public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> {
+
+    Context context;
+    List<Movie> movies;
+
+    public MovieAdapter(Context context, List<Movie> movies) {
+        this.context = context;
+        this.movies = movies;
+    }
+
+
+    @NonNull
+    @Override
+
+    //inflates the layout and returns the holder
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Log.d("MovieAdapter", "onCreateViewHolder");
+        View movieView = LayoutInflater.from(context).inflate(R.layout.item_movie, parent, false);
+        return new ViewHolder(movieView);
+    }
+
+    //Populates data into item in the holder
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Log.d("MovieAdapter", "onBindViewHolder" + position);
+        //Get movie at the passed position
+        Movie movie = movies.get(position);
+        //Bind movie data into the View Holder
+        holder.bind(movie);
+    }
+
+    //get the total count of items
+    @Override
+    public int getItemCount() {
+        return movies.size();
+    }
+
+    //View holder represents the item (Movie)
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        TextView tvTitle;
+        TextView tvOverview;
+        ImageView ivPoster;
+        RelativeLayout container;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tvTitle = itemView.findViewById(R.id.tvTitle);
+            tvOverview = itemView.findViewById(R.id.tvOverview);
+            ivPoster = itemView.findViewById(R.id.ivPoster);
+            container = itemView.findViewById(R.id.container);
+        }
+
+        public void bind(Movie movie) {
+            tvTitle.setText(movie.getTitle());
+            tvOverview.setText(movie.getOverview());
+            Glide.with(context).load(movie.getPosterPath()).into(ivPoster);
+
+            //register click listener to whole container
+            //Go to a new activity once clicked
+            container.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(context, "onClick", Toast.LENGTH_SHORT).show();
+                }
+            });
+
+        }
+    }
+}
